9a609eaba2a5b1fd06ceb97260d1d9962dcff112
johnsonakpama <johnsonakpama33@gmail.com>
2024-04-24 13:07:45 +0100

    prep 2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percent?");
+        string answer = Console.RreadLine();
+        int percent = int.parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You pass!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
