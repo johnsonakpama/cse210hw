b326c82e42a24c6ec7f1e405846cdca819ce89f8
johnsonakpama <johnsonakpama33@gmail.com>
2024-05-21 14:55:57 +0100

    Week 5 learning activity done

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,23 @@
+using system;
+
+public abstract class Shape
+{
+    private string _color;
+
+    public string Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+}
